88c73569ae70066cceb36522b5ba9371f7d331d5
冯文春 <707908885@qq.com>
2014-09-21 20:06:24 +0800

    GitTest.java添加id属性 ，测试冲突

--- aabb/src/com/tdrz/test/GitTest.java
+++ aabb/src/com/tdrz/test/GitTest.java
@@ -8,6 +8,16 @@ public class GitTest {
 	
 	private String gender;
 	
+	private String id;
+	
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public String getGender() {
 		return gender;
