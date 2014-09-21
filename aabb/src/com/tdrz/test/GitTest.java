6be3be8b28dd016316939c12c132b58a0d246b79
冯文春 <707908885@qq.com>
2014-09-21 15:28:16 +0800

    为username和password添加set和get方法

--- aabb/src/com/tdrz/test/GitTest.java
+++ aabb/src/com/tdrz/test/GitTest.java
@@ -5,7 +5,22 @@ public class GitTest {
 	private String username;
 	
 	private String password;
-	
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	
 	
 	
